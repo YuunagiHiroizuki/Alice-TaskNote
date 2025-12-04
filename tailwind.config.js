e8fb4896803be7c572684e95cb99ad4539ba5176
YuunagiHiroizuki <1223152956@qq.com>
2025-12-04 21:21:11 +0800

    修复md不渲染的问题

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+@type {import('tailwindcss').Config} 
 export default {
   content: [
     "./index.html",
@@ -7,8 +7,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
   corePlugins: {
-    preflight: false // **重要**: 防止Tailwind重置Element Plus的样式
+    preflight: false
   }
 }
